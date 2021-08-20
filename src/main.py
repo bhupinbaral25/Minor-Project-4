ca808abd6793d9c8dc13bbd6b94d11f8f0708797
Bhupin Baral <bhupinbaral.729@gmail.com>
2021-08-20 20:31:31 +0545

    testing different model in test data

--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import numpy as np 
 from sentence_transformers import SentenceTransformer
-from utils import get_sentence_embeddings
-
-corpus = ['My name is Bhupin Baral','I live in Kathmandu Nepal']
-
-models = ['paraphrase-MiniLM-L6-v2', 'nli-distilroberta-base-v2']
-for model in models:
-
-    model = SentenceTransformer(model)
-    sentence_embeddings = get_sentence_embeddings(model, corpus)
-print(sentence_embeddings)
-
+from utils import get_sentence_embeddings, get_cosine_similarity
 
 df = pd.read_csv('../data/sts_test.csv')
 
-print(df)
+
+models = ['paraphrase-MiniLM-L6-v2', 
+        'nli-distilroberta-base-v2']
+similarity_score_dict = { }
+for model in models:
+    model = SentenceTransformer(model)
+    for  index in range(len(df)):
+        similarity_score = get_cosine_similarity(get_sentence_embeddings(model, list(df['sent_2']))[index], 
+                                                get_sentence_embeddings(model, list(df['sent_1']))[index])
+    
+    similarity_score_dict[model] = similarity_score
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,41 @@
-from sentence_transformers import SentenceTransformer
-model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
-
-from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+from sentence_transformers import  util, SentenceTransformer
+'''
+input = model name in list (multiple model)
+        corpus in list 
+output = list of embeddings
+'''
 
 def get_sentence_embeddings(model, corpus : list ):
     
     return model.encode(corpus)
+'''
+input = list of sentence ebeddings
+output = similarity score
+'''
 
-def get_cosine_similarity(corpous_vector : list, sentence_vector : list):
+def get_similarity_score(corpus_vector, sentence_vector):
 
-    return(cosine_similarity(
-    [corpous_vector],
-    [sentence_vector])
-)
+    return(util.pytorch_cos_sim(corpus_vector, sentence_vector))
 
+'''
+input = model to test, test dataset, corpus, sentence 
+output = similarity score in list
+'''
+
+def test_model(models, test_data, corpus, sentence):
+    similarity_score_dict = { }
+    for model in models:
+        model_name = model
+        model = SentenceTransformer(model)
+
+        corpus_embedding = get_sentence_embeddings(model, list(test_data[corpus]))
+
+        sentence_embedding = get_sentence_embeddings(model, list(test_data[sentence]))
+
+        for  index in range(len(test_data[corpus])):
+        
+            similarity_score = np.array(util.pytorch_cos_sim(corpus_embedding[index], sentence_embedding[index]))                                   
+            similarity_score_dict[model_name] = similarity_score
+    
+    return similarity_score
