3754ba5d2293ce4dc1c2d741d817404e92df476e
Bhupin Baral <bhupinbaral.729@gmail.com>
2021-08-21 21:38:45 +0545

    update issue and get similarity score

--- src/utils.py
+++ src/utils.py
@@ -1,41 +1,48 @@
 import numpy as np
 from sentence_transformers import  util, SentenceTransformer
-'''
-input = model name in list (multiple model)
-        corpus in list 
-output = list of embeddings
-'''
+from sklearn.metrics.pairwise import cosine_similarity
 
-def get_sentence_embeddings(model, corpus : list ):
-    
+
+def get_sentence_embeddings(model, corpus : list ) :
+    '''
+    input = model name in list (multiple model)
+            corpus in list 
+    output = list of embeddings
+    '''
+
     return model.encode(corpus)
-'''
-input = list of sentence ebeddings
-output = similarity score
-'''
+
 
 def get_similarity_score(corpus_vector, sentence_vector):
 
+    '''
+    input = list of sentence ebeddings
+    output = similarity score
+    '''
+
     return(util.pytorch_cos_sim(corpus_vector, sentence_vector))
 
-'''
-input = model to test, test dataset, corpus, sentence 
-output = similarity score in list
-'''
 
-def test_model(models, test_data, corpus, sentence):
-    similarity_score_dict = { }
+def test_model(models, dataframe, corpus, sentence):
+
+    '''
+    input = model to test, test dataset, corpus, sentence 
+    output = similarity score in list
+    '''
+
+    similarity_score_dict, similarity_score= { },[]
     for model in models:
         model_name = model
         model = SentenceTransformer(model)
 
-        corpus_embedding = get_sentence_embeddings(model, list(test_data[corpus]))
+        corpus_embedding = get_sentence_embeddings(model, list(dataframe[corpus]))
 
-        sentence_embedding = get_sentence_embeddings(model, list(test_data[sentence]))
+        sentence_embedding = get_sentence_embeddings(model, list(dataframe[sentence]))
 
-        for  index in range(len(test_data[corpus])):
+        for  index in range(len(corpus_embedding)):
         
-            similarity_score = np.array(util.pytorch_cos_sim(corpus_embedding[index], sentence_embedding[index]))                                   
-            similarity_score_dict[model_name] = similarity_score
+            similarity_score.append(np.array(util.pytorch_cos_sim(corpus_embedding[index], sentence_embedding[index])))
+                                         
+            similarity_score_dict[model_name] = np.array(similarity_score)
     
-    return similarity_score
+    return similarity_score_dict
