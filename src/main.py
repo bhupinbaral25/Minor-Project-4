347785895be7f853a18ded3ba5c2b90c3cedd0b7
Bhupin Baral <bhupinbaral.729@gmail.com>
2021-08-21 21:56:01 +0545

    get similarity score as numpy array

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+from utils import test_model
+
+df = pd.read_csv('../data/sts_test.csv')
+
+models = ['paraphrase-MiniLM-L6-v2', 
+        'nli-distilroberta-base-v2']
+
+similarity_score = test_model(models, df, 'sent_1', 'sent_2')
+
+print(similarity_score)
